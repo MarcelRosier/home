ba3a4ef493d9d60e9d8ae9ed35c8087d9c913011
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-15 22:51:22 +0200

    Fix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
   },
 
   integrations: [mdx(), react(), icon()],
-  site: "https://MarcelRosier.github.io",
+  site: "https://marcelrosier.github.io",
   base: "/home",
 });
