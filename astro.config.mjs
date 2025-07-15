fc230964ec56a76b140b2a8e2fc907fb8fe812b2
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-15 13:32:57 +0200

    Add libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()],
 });
