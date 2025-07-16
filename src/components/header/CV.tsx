2d6793aea9926b44b81158d6efd2f5b8767f296a
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-16 23:00:09 +0200

    Add cv

---
+++ src/components/header/CV.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { FileText } from "lucide-react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import { Button } from "@/components/ui/button";
+
+export function CV() {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">
+          <Button size="icon" variant="ghost">
+            <FileText />
+          </Button>
+        </a>
+      </TooltipTrigger>
+      <TooltipContent>
+        <div className="flex items-center gap-x-1 text-sm">Checkout my CV</div>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
