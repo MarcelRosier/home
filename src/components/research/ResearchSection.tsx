e8ea70982f0a707926f0d2272263d5eb472816ba
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-23 22:18:18 +0200

    Change to animate in when in view

--- src/components/research/ResearchSection.tsx
+++ src/components/research/ResearchSection.tsx
@@ -5,16 +5,6 @@ type Props = {
   entries: any;
 };
 
-const containerVariants = {
-  hidden: {},
-  show: {
-    transition: {
-      staggerChildren: 0.15,
-      delayChildren: 0.2,
-    },
-  },
-};
-
 const itemVariants = {
   hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0, transition: { duration: 0.4, ease: easeOut } },
@@ -24,18 +14,19 @@ export default function ResearchList({ entries }: Props) {
   return (
     <section className="mt-10">
       <h2 className="mb-8 text-3xl">Research</h2>
-      <motion.div
-        className="flex flex-col space-y-8 2xl:space-y-12"
-        variants={containerVariants}
-        initial="hidden"
-        animate="show"
-      >
+      <div className="flex flex-col space-y-8 2xl:space-y-12">
         {entries.map((entry, index) => (
-          <motion.div variants={itemVariants} key={index}>
+          <motion.div
+            variants={itemVariants}
+            key={index}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.2 }}
+          >
             <ResearchEntry researchEntry={entry} />
           </motion.div>
         ))}
-      </motion.div>
+      </div>
     </section>
   );
 }
