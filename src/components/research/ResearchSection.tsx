5b2021a089c8252ce507d6d9b1c63e16c86b5dd0
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-23 22:22:55 +0200

    Fade in h2 as well

--- src/components/research/ResearchSection.tsx
+++ src/components/research/ResearchSection.tsx
@@ -5,19 +5,27 @@ type Props = {
   entries: any;
 };
 
-const itemVariants = {
+const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
-  show: { opacity: 1, y: 0, transition: { duration: 0.4, ease: easeOut } },
+  show: { opacity: 1, y: 0, transition: { duration: 0.5, ease: easeOut } },
 };
 
 export default function ResearchList({ entries }: Props) {
   return (
     <section className="mt-10">
-      <h2 className="mb-8 text-3xl">Research</h2>
+      <motion.h2
+        className="mb-8 text-3xl"
+        variants={fadeInUp}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.3 }}
+      >
+        Research
+      </motion.h2>
       <div className="flex flex-col space-y-8 2xl:space-y-12">
         {entries.map((entry, index) => (
           <motion.div
-            variants={itemVariants}
+            variants={fadeInUp}
             key={index}
             initial="hidden"
             whileInView="show"
