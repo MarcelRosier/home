cad603b4db8c118c1310ed86a01dc373945d0b05
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-23 22:05:48 +0200

    Add basic animation to display list

--- src/components/research/ResearchSection.tsx
+++ src/components/research/ResearchSection.tsx
@@ -1,21 +1,41 @@
-import { motion } from "motion/react";
+import { motion, easeOut } from "motion/react";
 import ResearchEntry from "./ResearchEntry"; // ensure this is also a .tsx file
 
 type Props = {
   entries: any;
 };
 
+const containerVariants = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.15,
+      delayChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0, transition: { duration: 0.4, ease: easeOut } },
+};
+
 export default function ResearchList({ entries }: Props) {
   return (
     <section className="mt-10">
       <h2 className="mb-8 text-3xl">Research</h2>
-      <div className="flex flex-col space-y-8 2xl:space-y-12">
+      <motion.div
+        className="flex flex-col space-y-8 2xl:space-y-12"
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+      >
         {entries.map((entry, index) => (
-          <motion.div>
+          <motion.div variants={itemVariants} key={index}>
             <ResearchEntry researchEntry={entry} />
           </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 }
